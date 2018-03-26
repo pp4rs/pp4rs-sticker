cf7367bd80c0f1c7313e273655763c8e664fafb2
lachlan <lachlan.deer@uzh.ch>
2018-03-26 23:49:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hex Sticker for Programming Practices for Research in Economics
+
+* Maintainer: Lachlan Deer (@lachlandeer)
+
+##  What this repo does
+* This repo constructs a hex sticker for the pp4rs organization:
+
+![](./pp4rs.png)
---
+++ pp4rs.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+imgurl <- "https://camo.githubusercontent.com/b6804eabe04a1cdc45083bbb0865634fa881e765/68747470733a2f2f676f6f2e676c2f5671674d5a4a"
+sticker(imgurl, package="pp4rs", s_x=1, s_y=1.15, s_width=.8, p_y = 0.45, p_size = 20, h_fill = "#FFFFFF",
+        h_color = "#0C5B7A", p_color = "#0C5B7A", h_size = 1.4,
+        url = "github.com/pp4rs", u_size = 4, u_color = "#0C5B7A",
+        filename="pp4rs.png")
